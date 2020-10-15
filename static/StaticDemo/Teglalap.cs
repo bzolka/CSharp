63bf24e2172eca7978f390015e93f918d8e1e285
zbenedek <benedek.zoltan@mirango.hu>
2020-10-15 21:20:11 +0200

    static added

---
+++ static/StaticDemo/Teglalap.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticDemo
+{
+    namespace T1
+    {
+        class Teglalap
+        {
+            public double Szelesseg;
+            public double Magassag;
+
+            public double Terulet()
+            {
+                return Szelesseg * Magassag;
+            }
+        }
+    }
+
+    namespace T2
+    {
+        class Teglalap
+        {
+            public double Szelesseg;
+            public double Magassag;
+
+            public int Szamlalo;
+
+            public Teglalap()
+            {
+                Szamlalo++;
+            }
+
+            public double Terulet()
+            {
+                return Szelesseg * Magassag;
+            }
+        }
+    }
+
+
+    namespace T3
+    {
+        class Teglalap
+        {
+            public double Szelesseg;
+            public double Magassag;
+
+            public static int Szamlalo;
+
+            public Teglalap()
+            {
+                Szamlalo++;
+            }
+
+            public double Terulet()
+            {
+                return Szelesseg * Magassag;
+            }
+        }
+    }
+
+    namespace T4
+    {
+        class Teglalap
+        {
+            public double Szelesseg;
+            public double Magassag;
+
+            private static int Szamlalo;
+
+            public static int SzamlaloErtek()
+            {
+                return Szamlalo;
+            }
+
+            public Teglalap()
+            {
+                Szamlalo++;
+            }
+
+            public double Terulet()
+            {
+                return Szelesseg * Magassag;
+            }
+        }
+    }
+
+}
